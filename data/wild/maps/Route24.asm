d9cc990a8635f341f713649fa5a81df4628892a6
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-02-20 22:39:26 -0500

    Update Route24.asm

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -4,8 +4,8 @@ IF DEF(_RED)
 	db  7, WEEDLE
 	db  8, KAKUNA
 	db 12, PIDGEY
-	db 12, ODDISH
 	db 13, ODDISH
+	db 12, BELLSPROUT
 	db 10, ABRA
 	db 14, ODDISH
 ENDC
@@ -13,8 +13,8 @@ IF DEF(_BLUE)
 	db  7, CATERPIE
 	db  8, METAPOD
 	db 12, PIDGEY
-	db 12, BELLSPROUT
 	db 13, BELLSPROUT
+	db 12, ODDISH
 	db 10, ABRA
 	db 14, BELLSPROUT
 ENDC
@@ -25,3 +25,4 @@ ENDC
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
