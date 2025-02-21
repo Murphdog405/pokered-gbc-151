7d613a2ccef4421401e4ea967f709261cb877639
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-02-20 22:44:17 -0500

    Update Route12.asm

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -5,26 +5,27 @@ IF DEF(_RED)
 	db 25, PIDGEY
 	db 23, PIDGEY
 	db 24, VENONAT
-	db 22, ODDISH
+	db 22, BELLSPROUT
 	db 26, VENONAT
 	db 26, ODDISH
 	db 27, PIDGEY
-	db 28, GLOOM
 	db 30, GLOOM
+	db 28, WEEPINBELL
 ENDC
 IF DEF(_BLUE)
 	db 24, BELLSPROUT
 	db 25, PIDGEY
 	db 23, PIDGEY
 	db 24, VENONAT
-	db 22, BELLSPROUT
+	db 22, ODDISH
 	db 26, VENONAT
 	db 26, BELLSPROUT
 	db 27, PIDGEY
-	db 28, WEEPINBELL
 	db 30, WEEPINBELL
+	db 28, GLOOM
 ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
