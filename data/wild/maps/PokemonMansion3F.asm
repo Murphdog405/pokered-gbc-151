525adec45661a1767e6b0671ea56f9c876431c46
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-02-20 22:52:42 -0500

    Update PokemonMansion3F.asm

--- data/wild/maps/PokemonMansion3F.asm
+++ data/wild/maps/PokemonMansion3F.asm
@@ -1,25 +1,25 @@
 PokemonMansion3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 31, KOFFING
-	db 33, GROWLITHE
 	db 35, KOFFING
+	db 33, GROWLITHE
+	db 31, GRIMER
 	db 32, PONYTA
-	db 34, PONYTA
+	db 34, MAGMAR
 	db 40, WEEZING
-	db 34, GRIMER
+	db 33, VULPIX
 	db 38, WEEZING
 	db 36, PONYTA
 	db 42, MUK
 ENDC
 IF DEF(_BLUE)
-	db 31, GRIMER
-	db 33, VULPIX
 	db 35, GRIMER
+	db 33, VULPIX
+	db 31, KOFFING
 	db 32, PONYTA
 	db 34, MAGMAR
 	db 40, MUK
-	db 34, KOFFING
+	db 33, GROWLITHE
 	db 38, MUK
 	db 36, PONYTA
 	db 42, WEEZING
@@ -28,3 +28,4 @@ ENDC
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
