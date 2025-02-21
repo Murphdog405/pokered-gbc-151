e92c75db3b2299cf765d5837c0adad622b6548ab
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-02-20 22:53:15 -0500

    Update PokemonMansion2F.asm

--- data/wild/maps/PokemonMansion2F.asm
+++ data/wild/maps/PokemonMansion2F.asm
@@ -3,11 +3,11 @@ PokemonMansion2FWildMons:
 IF DEF(_RED)
 	db 32, GROWLITHE
 	db 34, KOFFING
-	db 34, KOFFING
+	db 34, GRIMER
 	db 30, PONYTA
-	db 30, KOFFING
+	db 32, VULPIX
 	db 32, PONYTA
-	db 30, GRIMER
+	db 32, KOFFING
 	db 28, PONYTA
 	db 39, WEEZING
 	db 37, MUK
@@ -15,11 +15,11 @@ ENDC
 IF DEF(_BLUE)
 	db 32, VULPIX
 	db 34, GRIMER
-	db 34, GRIMER
+	db 34, KOFFING
 	db 30, PONYTA
-	db 30, GRIMER
+	db 32, GROWLITHE
 	db 32, PONYTA
-	db 30, KOFFING
+	db 32, GRIMER
 	db 28, PONYTA
 	db 39, MUK
 	db 37, WEEZING
@@ -28,3 +28,4 @@ ENDC
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
