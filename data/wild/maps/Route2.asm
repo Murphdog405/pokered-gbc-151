41f949b5b513114ec2c6aed48b3bb18114ccfa14
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-02-20 22:41:34 -0500

    Update Route2.asm

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -8,18 +8,19 @@ Route2WildMons:
 IF DEF(_RED)
 	db  3, WEEDLE
 	db  2, RATTATA
-	db  5, RATTATA
-	db  4, WEEDLE
 	db  5, WEEDLE
+	db  4, CATERPIE
+	db  5, BULBASAUR
 ENDC
 IF DEF(_BLUE)
 	db  3, CATERPIE
 	db  2, RATTATA
-	db  5, RATTATA
-	db  4, CATERPIE
 	db  5, CATERPIE
+	db  4, WEEDLE
+	db  5, BULBASAUR
 ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
